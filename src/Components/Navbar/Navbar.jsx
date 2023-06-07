a0ab56203c153b3b251e32c3232ca806851f07dd
aminul-islam01 <abiddut953@gmail.com>
2023-06-07 23:44:21 +0600

    complite banner section

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,13 +11,13 @@ const Navbar = () => {
     </>
 
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-black bg-opacity-50 fixed top-0 left-0 z-10 text-white">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 text-black">
                         {navItems}
                     </ul>
                 </div>
--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,21 +1,52 @@
-import ReactPlayer from 'react-player';
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Pagination, Navigation } from "swiper";
+import banner1 from "../../../images/banner5.jpg"
+import banner2 from "../../../images/banner4.jpg"
+import banner3 from "../../../images/banner6.jpg"
+import banner4 from "../../../images/banner2.jpg"
+import banner5 from "../../../images/banner1.jpg"
+
 
 const Banner = () => {
+    const imgArray = [banner2, banner1, banner3, banner4, banner5];
+  
     return (
-        <div className="banner relative h-96">
-        <ReactPlayer
-          url="./video.mp4"
-          playing
-          loop
-          muted
-          width="100%"
-          height="100%"
-        />
-        {/* Rest of your banner content */}
-        <div className='absolute top-0 left-0 bg-slate-500 w-full h-full bg-opacity-70'>
-            <h2 className='text-3xl font-bold text-red-500'>aminul islam</h2>
-        </div>
-      </div>
+      <Swiper
+      spaceBetween={30}
+      centeredSlides={true}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Autoplay, Pagination, Navigation]}
+      className="mySwiper"
+    >
+      {imgArray.map((image, index) => 
+       <SwiperSlide key={index}><img className="w-full h-[100vh]" src={image} />
+       <div className="absolute top-0 left-0 w-full h-full flex items-center justify-between bg-black bg-opacity-50 p-10">
+         <div className="md:w-1/2 text-white">
+           <h2 className="text-4xl font-bold mb-5">Unleash Your Artistic Potential</h2>
+           <p>Welcome to DrawMasterClass, the premier online destination for aspiring artists and seasoned sketchers alike. Discover the joy of drawing as our expert instructors guide you through a transformative journey of artistic expression. From mastering the fundamentals to exploring diverse mediums, our comprehensive courses empower you to create captivating works of art. Join our vibrant community of creators and unlock your creativity today!<br/>
+           Feel free to customize and adapt the banner title and description to fit the specific focus and tone of your drawing school website.
+           </p>
+         </div>
+         <button className="btn btn-secondary">Enroll Now</button>
+       </div>
+       </SwiperSlide>
+      )}
+      
+      {/* <SwiperSlide><img className="w-full h-full" src={banner1} /></SwiperSlide>
+      <SwiperSlide><img className="w-full h-full" src={banner1} /></SwiperSlide>
+      <SwiperSlide><img className="w-full h-full" src={banner1} /></SwiperSlide> */}
+    </Swiper>
     );
 };
 
