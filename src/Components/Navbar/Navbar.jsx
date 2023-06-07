6d7663dacbd0d308ded70747d49d24760fd8a311
aminul-islam01 <abiddut953@gmail.com>
2023-06-08 01:20:12 +0600

    add login page

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,10 +8,11 @@ const Navbar = () => {
         <li><Link to="/instructor">Instructors</Link></li>
         <li><Link>Classes</Link></li>
         <li><Link>Dashboard</Link></li>
+        <li><Link to="/login">Login</Link></li>
     </>
 
     return (
-        <div className="navbar bg-black bg-opacity-50 fixed top-0 left-0 z-10 text-white">
+        <div className="navbar bg-black bg-opacity-50 fixed top-0 z-10 text-white container mx-auto">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,8 +1,49 @@
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const handleLogin = data => {
+        console.log(data)
+    }
+
     return (
-        <div>
-            
+        <div className='my-32'>
+            <div className='md:w-1/2 p-10'>
+                <form onSubmit={handleSubmit(handleLogin)} className='bg-white p-5 shadow-xl'>
+                <h2 className='font-bold text-2xl text-center'>Login</h2>
+                    <div className="form-control mb-5">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type='email' {...register('email', { required: true })} placeholder="email" className="input input-bordered" />
+                        {errors.email && <p className='text-red-600'>email is required.</p>}
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" {...register('password', { required: true })} placeholder="password" className="input input-bordered" />
+                        {errors.password && <p className='text-red-600'>password is required.</p>}
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button type="submit" className="btn bg-green-600 hover:bg-green-700">Login</button>
+                    </div>
+                    {/* <input {...register('firstName')} />
+                <input {...register('lastName', { required: true })} />
+                {errors.lastName && <p>Last name is required.</p>}
+                <input {...register('age', { pattern: /\d+/ })} />
+                {errors.age && <p>Please enter number for age.</p>} */}
+                    {/* <input type="submit" /> */}
+                </form>
+            </div>
         </div>
     );
 };
