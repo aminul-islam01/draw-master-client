735ca74b987d256b8207fd4fb6720f1786122854
aminul-islam01 <abiddut953@gmail.com>
2023-06-10 00:08:13 +0600

    add all instructor page

--- src/Pages/Instructors/Instructors.jsx
+++ src/Pages/Instructors/Instructors.jsx
@@ -1,9 +1,49 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import SectionTitle from "../../Components/SectionTitle/SectionTitle";
 
 
 const Instructors = () => {
+    const [instructors, setInstructors] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/instructors')
+            .then(res => res.json())
+            .then(data => setInstructors(data))
+    }, [])
+
     return (
-        <div>
-            <h2>our instructors</h2>
+        <div className="mb-20 mt-28">
+            <SectionTitle subHeading="Instructors" heading="All Instructors">
+            </SectionTitle>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead className="bg-amber-600 text-xl text-white">
+                        <tr>
+                            <th>#</th>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {instructors.map((instructor, index) =>
+                            <tr key={instructor._id}>
+                                <th>{index +1}</th>
+                                <td>
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-16 h-16">
+                                            <img src= {instructor.image} />
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>{instructor.name}</td>
+                                <td>{instructor.email}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
